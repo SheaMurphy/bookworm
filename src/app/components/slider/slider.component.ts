4e1362e2ca3deb425577abfb2c938cd8625cf312
Shea.Murphy <shea@nology.io>
2019-12-11 17:35:14 +0000

    Updated slider logic

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnChanges
+} from "@angular/core";
 
 @Component({
   selector: "app-slider",
   templateUrl: "./slider.component.html",
   styleUrls: ["./slider.component.scss"]
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit, OnChanges {
   @Input() max: number;
   @Output() handleInput = new EventEmitter();
+  value: number;
+
+  ngOnInit() {
+    this.initValue();
+  }
+
+  ngOnChanges() {
+    this.initValue();
+  }
+
+  initValue() {
+    this.value = Math.floor((0 + this.max) / 2);
+  }
+
+  handleSlide(slideValue: string) {
+    this.value = +slideValue;
+    this.handleInput.emit(this.value);
+  }
 }
